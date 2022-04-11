137d2c0390f98d36318a2abc31c84d26c9451fcf
Seulgi <hanny1226@naver.com>
2022-04-11 16:10:27 +0900

    class components => functional component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,30 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../styles/TodoItem.css';
 
-class TodoItem extends Component {
-  render() {
-    const { text, checked, id, onToggle, onRemove } = this.props;
-    //props 종류
-    //checked : 체크박스 상태
-    //id : todo고유아이디
-    //onToggle : 체크박스 on/off함수
+const TodoItem = (props) => {
+  const { text, checked, id, onToggle, onRemove } = props;
 
-    return (
-      <div className="todoItem" onClick={() => onToggle(id)}>
-        <div className="removeItem" onClick={(e) => {
-          e.stopPropagation(); // onToggle 이 실행되지 않고 onRemove만 실행됨
-          onRemove(id)} //&times : X 표시
-        }>&times;</div>
-        {/*css 유동적으로 설정 - Template literals */}
-        <div className={`todoText ${checked && 'checked'}`}>
-          <div>{text}</div>
-        </div>
-        {
-          checked && (<div className="checkMark">✓</div>)
-        }
+  return (
+    <div className="todoItem" onClick={() => onToggle(id)}>
+      <div className="removeItem" onClick={(e) => {
+        e.stopPropagation(); // onToggle 이 실행되지 않고 onRemove만 실행됨
+        onRemove(id)} //&times : X 표시
+      }>&times;</div>
+      {/*css 유동적으로 설정 - Template literals */}
+      <div className={`todoText ${checked && 'checked'}`}>
+        <div>{text}</div>
       </div>
-    );
-  }
-}
+      {
+        checked && (<div className="checkMark">✓</div>)
+      }
+    </div>
+  );
+};
 
 export default TodoItem;
